5d350901a41bf010a000f2efdf02612afc7b23a7
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-07-07 23:23:12 +0545

    PROLOG: Factorial of Number using Recursive function

---
+++ Knowledge Representation/factorial_using_recursion.pl
@@ -0,0 +1,11 @@
+% 3.Find the factorial of number using recursion in Prolog
+
+% Base case
+factorial(0, 1).
+
+% Recursive case
+factorial(N, F) :-
+    N > 0,
+    N1 is N - 1,
+    factorial(N1, F1),
+    F is N * F1.
